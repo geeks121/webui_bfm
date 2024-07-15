92e04110d9057b3f0af81c5cd53d398aacb2b209
geeks121 <yozhy999@gmail.com>
2024-07-16 00:21:22 +0700

    fix credentials missing clossing for php

--- files/www/auth/credentials.php
+++ files/www/auth/credentials.php
@@ -7,3 +7,5 @@ if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
 return [
     'username' => 'admin',
     'hashed_password' => '$2y$10$VC7N65gPAESPJWYv9JTMDeSj92pYtIzh5Pb5piGc3HHYotpx41a7K' // 12345
+];
+?>
