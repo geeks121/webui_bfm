7a3395510291c8036256f40ce3926dce386b289a
geeks121 <yozhy999@gmail.com>
2025-02-03 12:42:25 +0700

    change js and css path

--- files/www/tools/modpes.php
+++ files/www/tools/modpes.php
@@ -387,7 +387,7 @@
         </div>
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="../auth/js/materialize.min.js"></script>
     <script>
         function toggleTheme() {
             const body = document.body;
